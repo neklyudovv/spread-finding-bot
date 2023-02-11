3175a0b6a99c96bf39bb0ca5da15e48ff2891748
neklyudovv <igor.nekludovv@gmail.com>
2023-02-12 01:56:16 +0300

    добавил возможность отслеживать несколько тикеров одновременно + останавливать отслеживания

--- bot.py
+++ bot.py
@@ -6,9 +6,8 @@ import os
 from dotenv import load_dotenv
 from time import sleep
 
-WAIT_FOR_ITEM, WAIT_FOR_LINK, WAIT_FOR_TRACK_END, WHICH_TO_STOP = range(4)
-global is_active
-is_active = 0
+WAIT_FOR_ITEM, WAIT_FOR_TICKER, WAIT_FOR_TRACK_END, WHICH_TO_STOP = range(4)
+
 
 def get_prices(ticker, sites=['bybit', 'exmo', 'binance', 'huobi']):
 	prices = {}
@@ -24,7 +23,7 @@ def get_prices(ticker, sites=['bybit', 'exmo', 'binance', 'huobi']):
 	return prices
 
 
-def track(update: Update, context: CallbackContext, ticker='', sites=[]):
+def track(update: Update, context: CallbackContext, ticker='', track_id = 0):
 	try:
 		prices = get_prices(ticker)
 	except:
@@ -36,51 +35,58 @@ def track(update: Update, context: CallbackContext, ticker='', sites=[]):
 		s += f'{p[i]} : {prices[p[i]]}\n'
 
 	context.bot.sendMessage(chat_id=update.message.from_user['id'], text=s)
-	while is_active != 0: # пока отслеживание активно
-		sleep(2)
-		prices = get_prices(ticker) # получаем свежие данные
-		s = ''
-		for i in range(0, len(prices)):
-			for j in range(1, len(prices)):
-				*p, = prices
-				first = float(prices[p[i]])
-				second = float(prices[p[j]])
-				if max(first, second) > min(first, second):
-					if ((max(first, second) - min(first, second))/min(first, second))*100 >= 2:
-						s += f'{((max(first, second) - min(first, second))/min(first, second))*100}% : {first} ({p[i]}) - {second} ({p[j]}) \n'
-		if s != '': # оповещаем пользователя при изменении
-			context.bot.sendMessage(chat_id=update.message.from_user['id'], text=s)
+	try:
+		while context.user_data['is_active'][track_id] != 0: # пока отслеживание активно
+			sleep(2)
+			prices = get_prices(ticker) # получаем свежие данные
+			s = ''
+			for i in range(0, len(prices)):
+				for j in range(1, len(prices)):
+					*p, = prices
+					first, second = float(prices[p[i]]), float(prices[p[j]])
+					if max(first, second) > min(first, second):
+						if ((max(first, second) - min(first, second))/min(first, second))*100 >= 2:
+							s += f'{((max(first, second) - min(first, second))/min(first, second))*100}% : {first} ({p[i]}) - {second} ({p[j]}) \n'
+			if s != '': # оповещаем пользователя при изменении
+				context.bot.sendMessage(chat_id=update.message.from_user['id'], text=s)
+	except:
+		print('stopped')
+		return None
 	return None 
 
 
 def start(update: Update, context: CallbackContext): # стартовая функция
+	if 'is_active' not in context.user_data.keys():
+		context.user_data['is_active'] = []
 	update.message.reply_text(text='trading tool bot - бот для нахождения разницы в 2% в курсах монет на разных биржах. Просто пришли мне тикер в формате BTC/USDT и я начну отслеживать изменения')
-	return WAIT_FOR_LINK # запускает ожидание ссылки от пользователя
+	return WAIT_FOR_TICKER # запускает ожидание ссылки от пользователя
 
 
-# def entry(update: Update, context: CallbackContext): # добавить новое отслеживание в случае если бот уже запущен
-# 	update.message.reply_text(text='Пришли мне тикер, который хочешь отслеживать')
-# 	return WAIT_FOR_LINK # запускает ожидание тикера от пользователя
-
-
-def wait_for_link(update, _): # обработка ожидания тикера
-	global ticker
+def wait_for_ticker(update, context): # обработка ожидания тикера
 	ticker = update.message.text
 	sites = ''
-	global is_active
-	is_active = 1
+	print(context.user_data['is_active'])
+	context.user_data['is_active'].append(1)
 	update.message.reply_text(text='Отлично! Я начал отслеживать '+ ticker + ' на следующих биржах:')# + {sites})
-	track(update, _, ticker)
-	return cancel(update, _)
-	#return WAIT_FOR_ITEM
+	track(update, context, ticker, len(context.user_data['is_active'])-1)
+	return cancel(update, context)
 
-	
 
-def cancel(update, _): # закрывает все ветки
-	global is_active
-	is_active = 0
-	#update.message.reply_text(text='stopped')
+def cancel(update, context):
 	return ConversationHandler.END
+
+
+def stop(update, context): # закрывает все ветки
+	msg = update.message.text
+	if msg.replace(' ', '') == '/stop':
+		update.message.reply_text(context.user_data['is_active'])
+	else:
+		track_id = msg.replace('/stop', '').replace(' ', '')
+		context.user_data['is_active'][int(track_id)-1] = 0
+		update.message.reply_text(text=f'Отслеживание #{track_id} остановлено')
+		#update.message.reply_text(text='stopped')
+		#update.message.reply_text(text='incorrect data')
+	#return ConversationHandler.END
 
 
 def main():
@@ -90,7 +96,7 @@ def main():
 		load_dotenv(dotenv_path)
 	ACCESS_TOKEN = os.getenv("TOKEN")
 	updater = Updater(token=ACCESS_TOKEN, use_context=True)
-	stop_handler = CommandHandler('stop', cancel)
+	stop_handler = CommandHandler('stop', stop)
 	updater.dispatcher.add_handler(stop_handler)
     #start_handler = CommandHandler('start', start)
     #updater.dispatcher.add_handler(start_handler)  # start handler
@@ -102,8 +108,8 @@ def main():
 			entry_points=[CommandHandler('start', start)],
 	        # этапы разговора, каждый со своим списком обработчиков сообщений
 			states={
-			WAIT_FOR_LINK: [MessageHandler(Filters.all, wait_for_link)],
-	           # WAIT_FOR_ITEM: [MessageHandler(Filters.all, wait_for_item)],        
+			WAIT_FOR_TICKER: [MessageHandler(Filters.all, wait_for_ticker)],
+	        #WAIT_FOR_SITES: [MessageHandler(Filters.all, wait_for_item)],        
 			},
 	        # точка выхода из разговора
 			fallbacks=[CommandHandler('cancel', cancel)], 
