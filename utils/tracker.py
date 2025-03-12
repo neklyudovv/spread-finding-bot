e4c485ad9c385df145ebedc9f6db810c0a406d99
Igor Nekludov <igor.nekludovv@gmail.com>
2025-03-12 22:58:37 +0300

    fixed if on spread

--- utils/tracker.py
+++ utils/tracker.py
@@ -6,7 +6,6 @@ from utils.parser import get_prices
 async def track_price(update, context, ticker):
     while context.user_data.get('is_active', False):
         prices = get_prices(ticker, cfg_exchanges)
-        print(prices)
         message = ""
         exchanges = list(prices.keys())
         try:
@@ -14,10 +13,10 @@ async def track_price(update, context, ticker):
                 for j in range(i + 1, len(exchanges)):
                     first, second = float(prices[exchanges[i]]), float(prices[exchanges[j]])
                     maxprice, minprice = max(first, second), min(first, second)
-                    spread = (maxprice - minprice) / minprice * 100 >= 2
-                    # print(spread)
+                    spread = (maxprice - minprice) / minprice * 100
                     if spread >= 2:
-                        message += f"Разница {exchanges[i]} - {exchanges[j]}: {maxprice - minprice:.2f} ({(maxprice - minprice) / minprice * 100:.2f}%)\n"
+                        message += (f"Разница {exchanges[i]} - {exchanges[j]}: {maxprice - minprice:.2f}"
+                                    f" ({spread:.2f}%)\n")
         except:
             await update.message.reply_text("no such ticker // smth went wrong")
             break
